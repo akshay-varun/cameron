33bb5f304da97e5b72042ca9db35d58df99e3e76
akshay-varun <akshaygr4@gmail.com>
2020-09-06 15:50:47 +0530

    Initial commit

---
+++ dump.sql
@@ -0,0 +1,3 @@
+create database cameron;
+CREATE USER 'cameron'@'localhost' IDENTIFIED WITH mysql_native_password BY 'fem&65n$78@aIOPu*j!248';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABL ES, LOCK TABLES ON feynman.* TO 'cameron'@'localhost';
